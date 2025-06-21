3b71c015ff83ace98a9bb5a4fbd1daa1557b96c1
ericdummyrepos <kirk02325@gmail.com>
2025-06-20 23:32:45 -0700

    Update S.java

--- S.java
+++ S.java
@@ -21,3 +21,38 @@ public class Employee {
         return hourlyRate;
     }
 }
+
+public class SalaryCalculator {
+    public double calculateSalary(Employee employee) {
+        return employee.getHoursWorked() * employee.getHourlyRate();
+    }
+}
+
+public class ReportPrinter {
+    public void printReport(Employee employee, double salary) {
+        System.out.println("Employee: " + employee.getName());
+        System.out.println("Total Salary: $" + salary);
+    }
+}
+
+public class EmployeeRepository {
+    public void save(Employee employee) {
+        System.out.println("Saving employee " + employee.getName() + " to database...");
+    }
+}
+
+// Main example
+public class Main {
+    public static void main(String[] args) {
+        Employee emp = new Employee("Alice", 40, 25.0);
+
+        SalaryCalculator calculator = new SalaryCalculator();
+        double salary = calculator.calculateSalary(emp);
+
+        ReportPrinter printer = new ReportPrinter();
+        printer.printReport(emp, salary);
+
+        EmployeeRepository repo = new EmployeeRepository();
+        repo.save(emp);
+    }
+}
